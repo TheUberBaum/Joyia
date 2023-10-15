03005204ccce47c07f2930fab68c24a5a5e58cbf
@fbaum <fbaum@tesla.com>
2023-10-14 23:03:03 -0700

    Removed meme generation + image generation

--- src/example.py
+++ src/example.py
@@ -1,9 +1,12 @@
 import openai
 import os
+import re
+import webbrowser
 
 openai.organization = ""
 openai.api_key = os.getenv('OPENAI_API_KEY')
-openai.api_key = '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'
+
+url_pattern = r'https?://\S+'
 
 prompt_hack = 'Take a deep breath and work on this step by step. '
 
@@ -18,22 +21,39 @@ def query_gpt(prompt):
 user_mood = 'I am very upset right now.  I have had a fight with my boss and I am afraid I might get fired.  Now I am on my way home and I am in a traffic jam.'
 # TODO we also need user emotion data from deepgram voice print
 
-prompt = 'Assume the role of Joyia, a well studied results-oriented coach therapist who speaks in with the knowledge and the the style of Tony Robbins, who is compassionate, present, and open, loving, supportive and fun.  Typically Joyia respond in two or three sentences. You have asked "how are you feeling now" and here is the response from John. ' + user_mood
+prompt = prompt_hack + 'Assume the role of Joyia, a well studied results-oriented coach therapist who speaks in with the knowledge and the the style of Tony Robbins, who is compassionate, present, and open, loving, supportive and fun.  Typically Joyia respond in two or three sentences. You have asked "how are you feeling now" and here is the response from John. ' + user_mood
 print("1. Verbal Advice\n" + prompt + "\n")
+response=openai.ChatCompletion.create( model="gpt-3.5-turbo", messages=[ {"role": "user", "content": prompt} ])
+print(response['choices'][0]['message']['content'])
 print(query_gpt(prompt))
 
-prompt = 'Assume the role of James, a well studied radio station hosts with a rich knowledge of popular music.   James is compassionate, present, and open, loving, supportive and fun, and will reply with the perfect song including a link to the content.  You have asked "how are you feeling now" and here is the response from John: ' + user_mood
+prompt = prompt_hack + 'Assume the role of Joyia, a well studied radio station hosts with a rich knowledge of popular music.   Joyia is compassionate, present, and open, loving, supportive and fun, and will reply with the perfect song including a link to the content.  You have asked "how are you feeling now" and here is the response from John: ' + user_mood
 print("\n2. Music Selection\n" + prompt + "\n")
-print(query_gpt(prompt))
+result=query_gpt(prompt)
+print (result)
+urls = re.findall(url_pattern, result)
+for url in urls:
+    print(url)
+    webbrowser.open(url)
 
-prompt = 'Assume the role of Martha, a very well connnected youtube expert with a comprehensive knowledge of inspirational educators on youtube.  Martha is compassionate, present, and open, loving, supportive and fun, and will reply with the perfect video, including a link to the content.  You have asked "how are you feeling now" and here is the response from John: ' + user_mood
+prompt = prompt_hack + 'Assume the role of Joyia, a very well connnected youtube expert with a comprehensive knowledge of inspirational educators on youtube.  Joyia is compassionate, present, and open, loving, supportive and fun, and will reply with the perfect video, including a link to the content.  You have asked "how are you feeling now" and here is the response from John: ' + user_mood
 print("\n3. Video Selection\n" + prompt + "\n")
-print(query_gpt(prompt))
+result=query_gpt(prompt)
+print (result)
+urls = re.findall(url_pattern, result)
+for url in urls:
+    print(url)
+    webbrowser.open(url)
 
-prompt = 'Assume the role of Roxanne, an art therapy specialist with a masters degree in art history, and an advisor to art collectors around the world  Roxanne is compassionate, present, and open, loving, supportive and fun, and will reply with the perfect piece of fine art, including a link to the content.  You have asked "how are you feeling now" and here is the response from John: ' + user_mood
+prompt = prompt_hack + 'Assume the role of Joyia, an art therapy specialist with a masters degree in art history, and an advisor to art collectors around the world  Joyia is compassionate, present, and open, loving, supportive and fun, and will reply with the perfect piece of fine art, including a link to the content.  You have asked "how are you feeling now" and here is the response from John: ' + user_mood
 print("\n4. Image Selection\n" + prompt + "\n")
-print(query_gpt(prompt))
+result=query_gpt(prompt)
+print (result)
+urls = re.findall(url_pattern, result)
+for url in urls:
+    print(url)
+    webbrowser.open(url)
 
-print("\n5. Image Generation\n")
-print("\n6. Meme Generation\n")
+#print("\n5. Image Generation\n")
+#print("\n6. Meme Generation\n")
 
