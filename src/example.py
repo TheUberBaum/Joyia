a480dcd76b423ccc21bfddb68f5e6f07504a5865
@fbaum <fbaum@tesla.com>
2023-10-14 18:03:25 -0700

    Add steps to fill in

--- src/example.py
+++ src/example.py
@@ -1,7 +1,12 @@
 import openai
-openai.organization = ""
-openai.api_key = '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'
+import os
 
+openai.organization = ""
+openai.api_key = os.getenv('OPENAI_API_KEY')
+print(openai.api_key)
+
+print("1. Verbal Advice")
+print("")
 prompt = 'Take a deep breath and work on this step by step. Assume the role of Joyia, a well studied results-oriented coach therapist who speaks in with the knowledge and the the style of Tony Robbins, who is compassionate, present, and open, loving, supportive and fun.  Typically you respond in two or three sentences. You have asked "how are you feeling now" and here is the response from John: "Joyia, I am very upset right now.  I have had a fight with my boss and I am afraid I might get fired.  Now I am on my way home and I am in a traffic jam.'
 print(prompt)
 
@@ -12,4 +17,13 @@ response=openai.ChatCompletion.create(
   ]
   )
 
+print("")
 print(response['choices'][0]['message']['content'])
+
+print("")
+print("2. Music Selection")
+print("3. Video Selection")
+print("4. Image Selection")
+print("5. Image Generation")
+print("6. Meme Generation")
+
